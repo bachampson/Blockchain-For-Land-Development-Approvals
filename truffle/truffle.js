cddce9b23b605cf30eb53e221fe71b63c591d33a
Chris Hampson <champson@businessaspect.com.au>
2018-07-21 15:30:40 +1000

    fixed vsts testing

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -1,7 +1,24 @@
+var TestRPC = require("ganache-cli");
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  mocha: {
+    reporter: "spec",
+    //reporter: "mocha-junit-reporter",
+    reporterOptions: {
+      mochaFile: 'TEST-truffle.xml'
+    }
+  },
   networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    // add a new network definition that will self host TestRPC
+    localtest: {
+      provider: TestRPC.provider(),
+      network_id:"*"
+    },
     ganache: {
       host: "127.0.0.1",
       port: 7545,
