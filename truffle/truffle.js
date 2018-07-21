9621312d6f2cbfc08f5771f5ee25cce27378ac51
Brad Turvey <braaad@gmail.com>
2018-07-21 11:47:06 +1000

    truffle

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -1,6 +1,3 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
-
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -14,15 +11,6 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    },
-    azure: {
-      host: "blchcu-dns-reg1.australiasoutheast.cloudapp.azure.com",
-      gas: 5000000,
-      port: 8545,
-      network_id: "10101010",
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://blchcu-dns-reg1.australiasoutheast.cloudapp.azure.com:8545", 0);
-      }
     }
   }
 };
