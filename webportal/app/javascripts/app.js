e3879b9744a8df7c2f129989813e1cfc15d83e24
Brad Turvey <braaad@gmail.com>
2018-07-21 11:46:16 +1000

    work

--- webportal/app/javascripts/app.js
+++ webportal/app/javascripts/app.js
@@ -114,18 +114,18 @@ window.App = {
 
     // Get the initial account balance so it can be displayed.
     web3.eth.getAccounts(function (err, accs) {
-      if (err != null) {
-        alert("There was an error fetching your accounts.");
-        return;
-      }
+      // if (err != null) {
+      //   alert("There was an error fetching your accounts.");
+      //   return;
+      // }
 
-      if (accs.length == 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-        return;
-      }
+      // if (accs.length == 0) {
+      //   alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+      //   return;
+      // }
 
-      accounts = accs;
-      account = accounts[0];
+      // accounts = accs;
+      // account = accounts[0];
 
     });
   },
